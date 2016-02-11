2a1364179418667245e4d5bc76f2c8182fc90d74
Rin <Rin@Rin-HP>
2016-02-11 15:12:03 -0800

    fixed add_item in both model and controller

--- application/models/Orders.php
+++ application/models/Orders.php
@@ -17,7 +17,19 @@ class Orders extends MY_Model
     // add an item to an order
     function add_item($num, $code) 
     {
-        
+        $CI =& get_instance();
+        if ($CI->orderitems->exists($num,$code))
+        {
+            $record = $CI->orderitems->get($num,$code);
+            $record->quantity++;
+            $CI->orderitems->update($record);
+        } else
+        {
+            $record = $CI->orderitems->create();
+            $record->order = $num;
+            $record->item = $code;
+            $CI ->orderitems->add($record);
+        }
     }
 
     // calculate the total for an order
