e96cc2e9c8c76962e1f6d1f6d7156b9ba72ca2ae
XCVG <xcvgvcx@gmail.com>
2016-02-11 14:45:55 -0800

    fixed order page display Signed-off-by:XCVG <xcvgvcx@gmail.com>

--- application/models/Orders.php
+++ application/models/Orders.php
@@ -5,36 +5,52 @@
  *
  * @author jim
  */
-class Orders extends MY_Model {
+class Orders extends MY_Model 
+{
 
     // constructor
-    function __construct() {
+    function __construct()
+    {
         parent::__construct('orders', 'num');
     }
 
     // add an item to an order
-    function add_item($num, $code) {
+    function add_item($num, $code) 
+    {
         
     }
 
     // calculate the total for an order
-    function total($num) {
-        return 0.0;
+    function total($num)
+    {
+        $CI = & get_instance();
+        $items = $CI->orderitems->group($num);
+        $result = 0;
+        if (count($items) > 0)
+            foreach ($items as $item)
+            {
+                $menu = $CI->menu->get($item->item);
+                $result += $item->quantity * $menu->price;
+            }
+        return $result;
     }
 
     // retrieve the details for an order
-    function details($num) {
+    function details($num)
+    {
         
     }
 
     // cancel an order
-    function flush($num) {
+    function flush($num)
+    {
         
     }
 
     // validate an order
     // it must have at least one item from each category
-    function validate($num) {
+    function validate($num)
+    {
         return false;
     }
 
