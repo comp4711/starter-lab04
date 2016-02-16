988ba142a0884defed4da52620728f6a8e0e776c
kwong425 <karlwong91@hotmail.com>
2016-02-15 20:52:41 -0800

    #7 controller/Order.php was recreated due to an error. Signed-off-by:kwong425 <karlwong91@hotmail.com>

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -5,22 +5,20 @@
  * Implement the different order handling usecases.
  * 
  * controllers/welcome.php
- *
+ * this file was recreated.
  * ------------------------------------------------------------------------
  */
-class Order extends Application {
+class Order extends Application 
+    {
     function __construct() {
         parent::__construct();
     }
     // start a new order
-    function neworder() {
-        //fixed order function
-        
-        $this->load->model('orders');
+    function neworder() 
+    {
         $order_num = $this->orders->highest() + 1;
         
         $neworder = $this->orders->create();
-        
         $neworder->num = $order_num;
         $neworder->date = date();
         $neworder->status = 'a';
@@ -30,14 +28,14 @@ class Order extends Application {
         redirect('/order/display_menu/' . $order_num);
     }
     // add to an order
-    function display_menu($order_num = null) {
-        if ($order_num == null) {
+    function display_menu($order_num = null) 
+        {
+        if ($order_num == null)
             redirect('/order/neworder');
-        }
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
-        //Fixed the title
-  $this->data['title'] = 'Order #' . $order_num.' ('.  number_format($this->orders->total($order_num),2).')';
+        $this->data['title'] = "Order #" . $order_num . ' (' . number_format($this->orders->total($order_num), 2) . ')';
+        
         // Make the columns
         $this->data['meals'] = $this->make_column('m');
         $this->data['drinks'] = $this->make_column('d');
@@ -61,53 +59,66 @@ class Order extends Application {
         $this->render();
     }
     // inject order # into nested variable pair parameters
-    function hokeyfix($varpair,$order) {
+    function hokeyfix($varpair,$order) 
+    {
 	foreach($varpair as &$record)
 	    $record->order_num = $order;
     }
     
     // make a menu ordering column
-    function make_column($category) {
-        
-       // fixed order display
-        $this->load->model('menu');
-      return $this->menu->some('category',$category);
+    function make_column($category) 
+    {
+        return $this->menu->some('category',$category);
     }
     // add an item to an order
-    function add($order_num, $item) {
-        //Fixed add
-          $this->load->model('orders');
-         $this->orders->add_item($order_num,$item);
+    function add($order_num, $item) 
+    {
+        $this->orders->add_item($order_num,$item);
         redirect('/order/display_menu/' . $order_num);
     }
     // checkout
-    function checkout($order_num) {
+    function checkout($order_num) 
+    {
         $this->data['title'] = 'Checking Out';
         $this->data['pagebody'] = 'show_order';
         $this->data['order_num'] = $order_num;
         
-        $this->data['total'] = number_format($this->orders->total($order_num), 2);
+        $this->data['total'] = number_format($this->orders->total($order_num),2);
         
         $items = $this->orderitems->group($order_num);
-        foreach ($items as $item){
+        foreach ($items as $item)
+        {
             $menuitem = $this->menu->get($item->item);
             $item->code = $menuitem->name;
         }
         $this->data['items'] = $items;
-
-        $this->data['okornot'] = $this->orders->validate($order_num) ? "" : "disabled";
-        //FIXME
-        $this->render();
         
+        $this->data['okornot'] = $this->orders->validate($order_num) ? "" : "disabled";
+        $this->render();
     }
     // proceed with checkout
-    function commit($order_num) {
-        //FIXME
+    function commit($order_num) 
+    {
+        if(!$this->orders->validate($order_num))
+        {
+            redirect('/order/display_menu/',$order_num);
+        }
+        $record = $this->orders->get($order_num);
+        $record->date = date(DATE_ATOM);
+        $record->status = 'c';
+        $record->total = $this->orders->total($order_num);
+        $this->orders->update($record);
+            
         redirect('/');
     }
     // cancel the order
-    function cancel($order_num) {
-        //FIXME
+    function cancel($order_num) 
+    {
+//        $this->orderitems->delete_some($order_num);
+        $this->orders->flush($order_num);
+        $record = $this->orders->get($order_num);
+        $record->status = 'x';
+        $this->orders->update($record);
         redirect('/');
     }
 }
