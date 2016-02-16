0898cc4106a693105fc3cdf503d989a7fce02ac9
Himesh Badlani <Himesh Badlani>
2016-02-16 13:16:29 -0800

    step 5 done

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -37,7 +37,7 @@ class Order extends Application {
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
         //FIXME
-
+		$order = $this->orders->get($order_num);
 		$this->data['title'] = 'Order # '.$order_num.' ('
                 .number_format($this->orders->total($order_num), 2).')';
         // Make the columns
@@ -79,6 +79,7 @@ class Order extends Application {
     // add an item to an order
     function add($order_num, $item) {
         //FIXME
+        $this->orders->add_item($order_num, $item);
         redirect('/order/display_menu/' . $order_num);
     }
 
