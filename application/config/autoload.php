28fe44f86189785391b440aaf0a9d963be7b3024
Renan Aguiar <renan@rca.pro.br>
2016-02-21 13:28:46 -0800

    fixed order adding fixed display total

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -137,4 +137,4 @@ $autoload['language'] = array();
 |	$autoload['model'] = array('first_model' => 'first');
 */
 
-$autoload['model'] = array('orders', 'menu');
+$autoload['model'] = array('orders', 'menu', 'orderitems');
